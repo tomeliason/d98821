77a71c014df35bae3446c57b38cc33fb4345286d
tom eliason <tom.eliason@oracle.com>
2016-10-21 12:57:10 -0600

    practices

---
+++ practices/admin/practice-07-7/describe_environment.py
@@ -0,0 +1,33 @@
+# By: ST Curriculum Development Team
+# Version 1.0
+# Last updated: May 22, 2013
+#
+# ------------------------------------------------------------------------
+# -- DISCLAIMER:
+# --    This script is provided for educational purposes only. It is NOT
+# --    supported by Oracle World Wide Technical Support.
+# --    The script has been tested and appears to work as intended.
+# --    You should always run new scripts on a test instance initially.
+# ------------------------------------------------------------------------
+
+# get operating system (for vars)
+import os
+
+# variables
+url = 't3://' + os.getenv('WLSAdminHost') + ':' + os.getenv('WLSAdminPort')
+username = os.getenv('WLSUsername')
+password = os.getenv('WLSPassword')
+
+print '>>>Connectiong to ' + url
+
+# Connect to administration server
+connect(username, password, url)
+
+# Print environment
+print version
+print domainName
+print serverName
+
+print '>>>Script completed successfully!'
+exit()
+
