0ccec3a38d3177fde5c83bcb7e6b619279f443c2
tom eliason <tom.eliason@oracle.com>
2016-10-27 15:57:48 -0600

    adding setup/reset

---
+++ practices/admin/practice-11-1/reset.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------
+# -- DISCLAIMER:
+# --    This script is provided for educational purposes only. It is NOT
+# --    supported by Oracle World Wide Technical Support.
+# --    The script has been tested and appears to work as intended.
+# --    You should always run new scripts on a test instance initially.
+# -- 
+# ------------------------------------------------------------------------
+
+# reset deploying an application with weblogic server console
+
+
+deployApplication_SimpleAuctionWebAppDb() {
+
+    echo "setting up ssh tunnel for WLST"
+    echo ssh -i ~/.ssh/id_rsa -f -N -T -M -L ${WLSDeployPort}:${JCSHost}:${WLSDeployPort} opc@remotehost-proxy
+
+    ssh -i ~/.ssh/id_rsa -f -N -T -M -L ${WLSDeployPort}:${JCSHost}:${WLSDeployPort} opc@remotehost-proxy
+
+curl -v -u ${WLSUsername}:${WLSPassword} -H "X-Requested-By:MyClient" -H Accept:application/json -H Content-Type:multipart/form-data -F "model={name:'SimpleAuctionWebAppDb',targets:['${WLSClusterName}']}" -F "deployment=@./SimpleAuctionWebAppDb.war" -X POST http://${WLSAdminHost}:${WLSDeployPort}/management/wls/latest/deployments/application
+
+    echo ssh -T -O "exit" remotehost-proxy
+    ssh -T -O "exit" remotehost-proxy
+    echo "terminating ssh tunnel for WLST"
+
+}
+
+# if this script is called as a main script, execute the function 
+if [ ${0##*/} == "reset.sh" ] ; then
+
+    #deployApplication_SimpleAuctionWebAppDb
+
+fi
