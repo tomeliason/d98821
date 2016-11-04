e83100d8455861c8d712882a110375fdb1dee33c
Al Saganich <al.saganich@oracle.com>
2016-11-04 10:04:13 -0400

    Added listServer.sh convinience script

---
+++ practices/admin/practice-09-3/listServers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------
+# -- DISCLAIMER:
+# --    This script is provided for educational purposes only. It is NOT
+# --    supported by Oracle World Wide Technical Support.
+# --    The script has been tested and appears to work as intended.
+# --    You should always run new scripts on a test instance initially.
+# -- 
+# ------------------------------------------------------------------------
+curl -v --user ${WLSUsername}:${WLSPassword} -H X-Requested-By:MyClient -H Accept:application/json -X GET http://${WLSAdminHost}:${WLSDeployPort}/management/weblogic/latest/domainRuntime/serverLifeCycleRuntimes?links=none
