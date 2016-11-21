30467258e509dd1bd43bfe6da0258bd6901fde80
tom eliason <tom.eliason@oracle.com>
2016-11-21 08:50:16 -0700

    updating

---
+++ practices/admin/practice-17-1/reset.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------
+# -- DISCLAIMER:
+# --    This script is provided for educational purposes only. It is NOT
+# --    supported by Oracle World Wide Technical Support.
+# --    The script has been tested and appears to work as intended.
+# --    You should always run new scripts on a test instance initially.
+# --
+# ------------------------------------------------------------------------
+
+#This script resets practice files, the domain, and the environment to its original state
+
+# if this script is called as a main script, execute the function 
+if [ ${0##*/} == "reset.sh" ] ; then
+
+        echo ">>> Resetting the practice environment for Practice 17-1"
+
+        echo ">>> "
+        
+        echo ">>> Practice 17-1 environment has been reset."
+
+fi
+
+
