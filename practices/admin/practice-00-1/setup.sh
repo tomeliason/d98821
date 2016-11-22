be24f16349cc129ae4cab0b1f1aa53a1b64a1afa
tom eliason <tom.eliason@oracle.com>
2016-11-22 07:16:02 -0700

    updating practice 00-01

---
+++ practices/admin/practice-00-1/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------
+# -- DISCLAIMER:
+# --    This script is provided for educational purposes only. It is NOT
+# --    supported by Oracle World Wide Technical Support.
+# --    The script has been tested and appears to work as intended.
+# --    You should always run new scripts on a test instance initially.
+# --
+# ------------------------------------------------------------------------
+
+# if this script is called as a main script, execute the function 
+if [ ${0##*/} == "setup.sh" ] ; then
+
+        echo ">>> Setting up the practice environment for Practice 00-1"
+        
+        echo ">>> Practice 00-1 environment has been setup."
+
+fi
---
+++ practices/admin/practice-00-1/solution.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------
+# -- DISCLAIMER:
+# --    This script is provided for educational purposes only. It is NOT
+# --    supported by Oracle World Wide Technical Support.
+# --    The script has been tested and appears to work as intended.
+# --    You should always run new scripts on a test instance initially.
+# --
+# ------------------------------------------------------------------------
+
+# environment variables:
+#   - JCSHost        - IP Address of the Admin Server
+#   - WLSAdminHost   - 
+#   - WLSDeployPort  - Port of the Admin Server - Administration Port
+#   - WLSUsername    - Admin User
+#   - WLSPassword    - Admin Password
+#   - WLSClusterName - Target Cluster
+
+practicedir=/practices/admin/practice-18-3
+
+# if this script is called as a main script, execute the function 
+if [ ${0##*/} == "solution.sh" ] ; then
+
+    echo ">>> Executing solution for Practice 00-1"
+
+    createPartition
+    deployShoppingCart
+        
+    echo ">>> The solution for Practice 00-1 has been completed."
+
+fi
+
